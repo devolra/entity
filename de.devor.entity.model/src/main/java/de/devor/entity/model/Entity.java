848f669be1c0ce493208fb76e9a9168e1f47204b
devor <mail.devor@gmail.com>
2018-02-02 19:14:01 +0100

    -Added missing Javadoc.

--- de.devor.entity.model/src/main/java/de/devor/entity/model/Entity.java
+++ de.devor.entity.model/src/main/java/de/devor/entity/model/Entity.java
@@ -10,24 +10,79 @@ import java.util.List;
  */
 public interface Entity {
 
+	/**
+	 * Returns the name,
+	 * 
+	 * @return The name.
+	 */
 	public String getName();
-	
+
+	/**
+	 * Sets the name.
+	 * 
+	 * @param name
+	 *            The name.
+	 */
 	public void setName(String name);
-	
+
+	/**
+	 * Returns the description.
+	 * 
+	 * @return The description.
+	 */
 	public String getDescription();
-	
+
+	/**
+	 * Sets the description.
+	 * 
+	 * @param description
+	 *            The description.
+	 */
 	public void setDescription(String description);
-	
+
+	/**
+	 * Returns the columns.
+	 * 
+	 * @return The columns.
+	 */
 	public List<Column> getColumns();
-	
+
+	/**
+	 * Adds a column.
+	 * 
+	 * @param column
+	 *            The column.
+	 */
 	public void addColumn(Column column);
-	
+
+	/**
+	 * Returns the primary key.
+	 * 
+	 * @return The primary key.
+	 */
 	public PrimaryKey getPrimaryKey();
-	
+
+	/**
+	 * Set primary key.
+	 * 
+	 * @param primaryKey
+	 *            The primary key.
+	 */
 	public void setPrimaryKey(PrimaryKey primaryKey);
-	
+
+	/**
+	 * Returns the indices.
+	 * 
+	 * @return The indices.
+	 */
 	public List<Index> getIndices();
-	
+
+	/**
+	 * Adds an index.
+	 * 
+	 * @param index
+	 *            The index.
+	 */
 	public void addIndex(Index index);
-	
+
 }
--- de.devor.entity.model/src/main/java/de/devor/entity/model/Index.java
+++ de.devor.entity.model/src/main/java/de/devor/entity/model/Index.java
@@ -10,16 +10,49 @@ import java.util.List;
  */
 public interface Index {
 
+	/**
+	 * Returs the name.
+	 * 
+	 * @return The name.
+	 */
 	public String getName();
 
+	/**
+	 * Sets the name.
+	 * 
+	 * @param name
+	 *            The name.
+	 */
 	public void setName(String name);
-	
+
+	/**
+	 * Returns if ascending.
+	 * 
+	 * @return true, if ascending, false if descending.
+	 */
 	public boolean isAscending();
-	
+
+	/**
+	 * Sets if ascending.
+	 * 
+	 * @param isAscending
+	 *            true, if ascending, false, if descendinhg.
+	 */
 	public void setAscending(boolean isAscending);
 
+	/**
+	 * Returns the columns.
+	 * 
+	 * @return The columns.
+	 */
 	public List<Column> getColumns();
 
+	/**
+	 * Adds a column.
+	 * 
+	 * @param column
+	 *            The column.
+	 */
 	public void addColumn(Column column);
 
 }
