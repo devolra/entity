544ad9829f6164d50f5730f91250d05b9bcdc485
devor <mail.devor@gmail.com>
2018-02-13 21:12:36 +0100

    Reverted previous changes.

--- de.devor.entity.pageflow/src/main/java/de/devor/entity/pageflow/entity/list/EntityListPageController.java
+++ de.devor.entity.pageflow/src/main/java/de/devor/entity/pageflow/entity/list/EntityListPageController.java
@@ -10,12 +10,4 @@ import de.devor.pageflow.mvc.core.PageController;
  */
 public interface EntityListPageController extends PageController {
 	
-	// This is a change that is done on Branch feature1.
-	// No changes happen on the master.
-	// Then this change is merged in the master branch.
-	
-	// This is another change on branch feature1.
-	// Changes happened on the master.
-	// Then this change is merged in the master.
-	
 }
