1f9d49d4e5cf392c64b0605880103f3c47c0b567
devor <mail.devor@gmail.com>
2018-02-16 21:16:28 +0100

    To test the triigering of the Jenkins job.

--- de.devor.entity.pageflow/src/main/java/de/devor/entity/pageflow/entity/list/EntityListPageController.java
+++ de.devor.entity.pageflow/src/main/java/de/devor/entity/pageflow/entity/list/EntityListPageController.java
@@ -12,5 +12,7 @@ public interface EntityListPageController extends PageController {
 
 	// A new feature to test a pull request a GitHub that triggers a Jenkins-Job to build the feature-branch and a merge
 	// from the feature-branch into the master.
+	
+	// Trigger the Jenkins-Job again.
 
 }
