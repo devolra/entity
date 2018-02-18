97e5113a470abd6cfed02df6b1d426e4f2885e90
devor <mail.devor@gmail.com>
2018-02-18 19:30:18 +0100

    Reverted the changes to test the GitHub Pull Request Builder - plugin.

--- de.devor.entity.pageflow/src/main/java/de/devor/entity/pageflow/entity/list/EntityListPageController.java
+++ de.devor.entity.pageflow/src/main/java/de/devor/entity/pageflow/entity/list/EntityListPageController.java
@@ -8,13 +8,6 @@ import de.devor.pageflow.mvc.core.PageController;
  * @author orapka
  *
  */
-public interface EntityListPageController extends PageController {
-
-	// This should be a new feature that is implemented on the feature branch feature-a
-	// This feature was integrated on the master.
-	
-	// This is another new feature on this feature branch that gets a new pull request.
-	
-	// And the last try to get a Jenkins build after a pull request was commited. 
+public interface EntityListPageController extends PageController { 
 	
 }
